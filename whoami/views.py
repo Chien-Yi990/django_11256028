1ae53124da73741ebaf32a8cb7f21b4fdfd9d437
Chien Yi <ian1230914@gmail.com>
2025-10-27 20:34:30 +0800

    新增首頁視圖，顯示學生資訊

--- whoami/views.py
+++ whoami/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("""台北商業大學
-
+    return HttpResponse("""
+<pre>
+台北商業大學
 五專資管科
-
 11256028
-
-簡翊安""")
+簡翊安
+</pre>
+""")
